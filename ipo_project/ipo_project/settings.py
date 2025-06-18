de6d5745a12152c98824cb823f4e5d1272b8b4ef
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-18 22:00:55 +0530

    Update database settings to use PostgreSQL instead of SQLite

--- ipo_project/ipo_project/settings.py
+++ ipo_project/ipo_project/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'ipo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ipo_db',              # You should create this in pgAdmin
+        'USER': 'postgres',            # your PostgreSQL username
+        'PASSWORD': 'admin123',        # your password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
