1a83a4df979a9786256df00062c174272e06f2a4
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-19 10:46:29 +0530

    Add homepage view and update URL patterns for root path

--- ipo_project/ipo_project/urls.py
+++ ipo_project/ipo_project/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def homepage(request):
+    return HttpResponse("<h1>Welcome to the IPO Web App 🚀</h1>")
+
 urlpatterns = [
+    path('', homepage),  # 👈 Now / will show a page
     path('admin/', admin.site.urls),
 ]
 
-# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
