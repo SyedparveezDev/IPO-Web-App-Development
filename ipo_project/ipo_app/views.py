e15c709f49bbfb3d933bf94b98944fda65941edd
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-19 11:36:25 +0530

    Add IPOCreateAPIView to handle creation of IPO data with admin permissions

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from .models import IPO
 from .serializers import IPOSerializer
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAdminUser
 
 class IPOListAPIView(APIView):
     def get(self, request):
@@ -13,3 +15,8 @@ class IPOListAPIView(APIView):
 class IPODetailAPIView(RetrieveAPIView):
     queryset = IPO.objects.all()
     serializer_class = IPOSerializer
+
+class IPOCreateAPIView(CreateAPIView):
+    queryset = IPO.objects.all()
+    serializer_class = IPOSerializer
+    permission_classes = [IsAdminUser]
