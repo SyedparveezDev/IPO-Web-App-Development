acda08572f483170dcf8915d8e5b9b4fe7fbb4ad
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-19 11:05:32 +0530

    Implement IPOListAPIView to handle GET requests for IPO data

--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import IPO
+from .serializers import IPOSerializer
 
-# Create your views here.
+class IPOListAPIView(APIView):
+    def get(self, request):
+        ipos = IPO.objects.all()
+        serializer = IPOSerializer(ipos, many=True)
+        return Response(serializer.data)
