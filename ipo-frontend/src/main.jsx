ab4c747202100ec7faadb2f8b1be2133524d3172
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-20 23:14:16 +0530

    Refactor main.jsx to implement routing with BrowserRouter and add IPOPage component

--- ipo-frontend/src/main.jsx
+++ ipo-frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import IPOPage from './IPOPage.jsx' // you'll create this next
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ipo/:id" element={<IPOPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
