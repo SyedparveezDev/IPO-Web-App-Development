30dd8c23fd4a2a464aeb843f587cf7a38a7770e7
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-23 19:46:35 +0530

    chore: Add CORS middleware and configuration in settings.py

--- ipo_project/setting.py
+++ ipo_project/setting.py
@@ -3,3 +3,21 @@ INSTALLED_APPS = [
     'corsheaders',
     'ipo_app',
 ]
+
+
+MIDDLEWARE = [
+    # Add your other middleware here, for example:
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+
