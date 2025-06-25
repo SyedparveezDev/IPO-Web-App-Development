fb2792c4147eb58ac6db99ae6c9b072ba3cdd528
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-25 17:49:16 +0530

    fix: Update database name and credentials in settings and setup script

--- scripts/setup_database.sql
+++ scripts/setup_database.sql
@@ -1,6 +1,6 @@
 -- Create database and user for IPO Admin
 CREATE DATABASE ipo_admin_db;
-CREATE USER ipo_admin_user WITH PASSWORD 'secure_password_123';
+CREATE USER ipo_admin_user WITH PASSWORD 'admin123';
 GRANT ALL PRIVILEGES ON DATABASE ipo_admin_db TO ipo_admin_user;
 
 -- Connect to the database
