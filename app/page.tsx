bd51379f0055ba0c66e3bb11733e2a8eebb5a18b
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-29 00:58:01 +0530

    refactor: Remove unused components and clean up page structure in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,2 @@
 "use client"
 
-
-
-export default function SyntheticV0PageForDeployment() {
-  return (
-    import Link from "next/link";
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome</h1>
-      <Link href="/login">Login</Link>
-      <Link href="/signup">Sign up</Link>
-    </main>
-  );
-}
-
-  )
-}
