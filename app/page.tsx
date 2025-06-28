7b5b3392c265005029e6adf89fa5b5072ac1ae1d
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-29 00:57:33 +0530

    fix: Correct component structure and update welcome message in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,17 @@
 
 export default function SyntheticV0PageForDeployment() {
   return (
+    import Link from "next/link";
+
+export default function Home() {
+  return (
     <main>
-      <h1>Hello, IPO App!</h1>
+      <h1>Welcome</h1>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Sign up</Link>
     </main>
+  );
+}
+
   )
 }
