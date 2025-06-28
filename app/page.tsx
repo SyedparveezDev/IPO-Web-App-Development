6efd446df4fb664cf274d96e44aa8aa16ad00443
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-29 00:58:13 +0530

    feat: Add Home component with welcome message and navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,13 @@
 "use client"
 
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Sign up</Link>
+    </main>
+  );
+}
