94e3c07fd83c2acee8fba0a3214c041e8df8598d
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-29 00:43:35 +0530

    chore: Comment out import statement for frontend test suite in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import  from "../static/js/frontend-test-suite"
+// import  from "../static/js/frontend-test-suite"
 
 export default function SyntheticV0PageForDeployment() {
   return < />
